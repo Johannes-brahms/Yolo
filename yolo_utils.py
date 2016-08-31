1319563bd5f8644af4df5e242a7b519188ce544b
jahannes-brahms <jahannes.brahms@icloud.com>
2016-08-31 13:31:58 +0800

    yolo

--- yolo_utils.py
+++ yolo_utils.py
@@ -50,13 +50,11 @@ def convert_to_one(bbox, width, height, S):
     cell_w = tf.cast(tf.div(tf.cast(width, tf.int32), S), tf.float32)
     cell_h = tf.cast(tf.div(tf.cast(height, tf.int32), S), tf.float32)
 
-
     cell_coord_x = tf.cast(tf.cast(tf.div(global_center_x, cell_w), tf.int32), tf.float32)
     cell_coord_y = tf.cast(tf.cast(tf.div(global_center_y, cell_h), tf.int32), tf.float32)
 
     offset_x = tf.div(tf.sub(global_center_x, tf.mul(cell_coord_x, cell_w)), cell_w)
     offset_y = tf.div(tf.sub(global_center_y, tf.mul(cell_coord_y, cell_h)), cell_h)
-
 
     assert offset_x.dtype == tf.float32 and \
             offset_y.dtype == tf.float32 and \
@@ -71,32 +69,46 @@ def convert_to_reality(bbox, width, height, S):
 
     relative_center_x, relative_center_y, global_w, global_h = bbox
 
-    #relative_center_x = tf.clip_by_value(relative_center_x, 0, 1)
-    #relative_center_y = tf.clip_by_value(relative_center_y, 0, 1)
-
-    w = tf.cast(tf.mul(global_w, width), tf.int32)
-    h = tf.cast(tf.mul(global_h, height), tf.int32)
-
-    cell_w = width / S
-    cell_h = height / S
+    w = tf.cast(tf.cast(tf.mul(global_w, width), tf.int32), tf.float32)
+    h = tf.cast(tf.cast(tf.mul(global_h, height), tf.int32), tf.float32)
 
     index = tf.reshape(tf.range(S * S),[-1,1])
 
-    cell_coord_y = tf.div(index, S)
-    cell_coord_x = tf.mod(index, S)
-
-    real_x_left_up = tf.sub(tf.add(tf.reshape(tf.mul(cell_coord_x, cell_w), [-1]), tf.cast(tf.mul(relative_center_x, cell_w), tf.int32)), tf.cast(tf.mul(tf.cast(w, tf.float32), 0.5), tf.int32))
-    real_y_left_up = tf.sub(tf.add(tf.reshape(tf.mul(cell_coord_y, cell_h), [-1]), tf.cast(tf.mul(relative_center_y, cell_h), tf.int32)), tf.cast(tf.mul(tf.cast(h, tf.float32), 0.5), tf.int32))
+    cell_coord_y = tf.cast(tf.div(index, S), tf.float32)
+    cell_coord_x = tf.cast(tf.mod(index, S), tf.float32)
 
 
-    real_x_left_up = tf.nn.relu(real_x_left_up)
-    real_y_left_up = tf.nn.relu(real_y_left_up)
+    S = tf.cast(S, tf.float32)
+
+    width = tf.cast(width, tf.float32)
+    height = tf.cast(height, tf.float32)
+
+
+    cell_w = tf.cast(width / S, tf.float32)
+    cell_h = tf.cast(height / S, tf.float32)
+
     
+    real_x_left_up = tf.reshape((cell_coord_x + relative_center_x) * cell_w - w / 2,[-1])
+
+    real_y_left_up = tf.reshape((cell_coord_y + relative_center_y) * cell_h - h / 2, [-1])
+
+    #real_x_left_up = tf.sub(tf.add(tf.reshape(tf.mul(cell_coord_x, cell_w), [-1]), tf.cast(tf.mul(relative_center_x, cell_w), tf.int32)), tf.cast(tf.mul(tf.cast(w, tf.float32), 0.5), tf.int32))
+    #real_y_left_up = tf.sub(tf.add(tf.reshape(tf.mul(cell_coord_y, cell_h), [-1]), tf.cast(tf.mul(relative_center_y, cell_h), tf.int32)), tf.cast(tf.mul(tf.cast(h, tf.float32), 0.5), tf.int32))
+
+
+    real_x_left_up = tf.cast(tf.nn.relu(real_x_left_up), tf.int32)
+    real_y_left_up = tf.cast(tf.nn.relu(real_y_left_up), tf.int32)
+    w = tf.cast(w, tf.int32)
+    h = tf.cast(h, tf.int32)
+
+
+
+    """
     assert real_x_left_up.dtype == tf.int32 and \
            real_y_left_up.dtype == tf.int32 and \
             w.dtype == tf.int32 and \
             h.dtype == tf.int32
-
+    """
     bbox = [real_x_left_up, real_y_left_up, w, h]
 
     return bbox
